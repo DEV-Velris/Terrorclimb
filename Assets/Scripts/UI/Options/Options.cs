c2d55adde4df00d73f8e03a3a27ca56de5318717
YAlexis <velris.dev@gmail.com>
2024-05-09 14:05:14 +0200

    Ajout ExtMeshPro

--- Assets/Scripts/UI/Options/Options.cs
+++ Assets/Scripts/UI/Options/Options.cs
@@ -5,8 +5,7 @@ public class Options : MonoBehaviour
 
     public void Back()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Main menu");
-        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("Options");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main menu");
     }
     
 }
